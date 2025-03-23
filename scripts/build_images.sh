9e87905291db12525dfa1ad2cd1294f05a6b856c
herjy <remyjoseph@ymail.com>
2025-03-24 00:19:47 +0100

    added docker build

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -7,7 +7,7 @@ tags=$souvenirs:latest
 
 echo "Building $people and $souvenirs with tags $tag"
 
-docker build --pull -t $tagp --target -f $people .
+docker build --pull --tag $tagp --target $people .
 docker push $tagp
-docker build --pull -t $tags --target -f $souvenirs .
+docker build --pull --tag $tags --target $souvenirs .
 docker push $tags

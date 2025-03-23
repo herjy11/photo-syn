3789de9e441a166e821d1bac1c6aff1fa2a0d741
herjy <remyjoseph@ymail.com>
2025-03-24 00:33:49 +0100

    added docker build

--- scripts/souvenirs.sh
+++ scripts/souvenirs.sh
@@ -4,7 +4,7 @@ name=$1
 
 run_name=syno.$name
 
-bash scripts/stop_container.sh $name
+bash scripts/stop_container.sh $run_name
 echo "Start container $run_name"
 sudo docker run -d --name=$run_name $name
 

e8e04d8f90ffc7dc92187a2241e47903968e67a7
herjy <remyjoseph@ymail.com>
2025-03-26 23:56:19 +0100

    dockerfile pip installs

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /photo-syn
 
 COPY . /photo-syn
 
-RUN pip install psycopg2-binary pandas numpy
 RUN pip install /photo-syn
 
 FROM base AS souvenirs
--- scripts/souvenirs.sh
+++ scripts/souvenirs.sh
@@ -6,6 +6,6 @@ run_name=syno.$name
 
 bash scripts/stop_container.sh $run_name
 echo "Start container $run_name"
-sudo docker run -d --name=$run_name $name
+sudo docker run -d --name=$run_name $name --network=host
 
 echo "Done"

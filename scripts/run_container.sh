442407e90a1df83e823e5e25c6a4025bb4ecc32f
herjy <remyjoseph@ymail.com>
2025-03-28 00:11:32 +0100

    full paths

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -6,6 +6,6 @@ run_name=syno.$name
 
 bash /var/services/homes/remy/git_repos/photo-syn/scripts/stop_container.sh $run_name
 echo "Start container $run_name"
-sudo -S docker run -d --network=host --name=$run_name -p 127.0.0.1:$3:$3 $name $2 $3
+docker run -d --network=host --name=$run_name -p 127.0.0.1:$3:$3 $name $2 $3
 
 echo "Done"
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -6,8 +6,8 @@ then
   exit 1
 fi
 
-sudo -S systemctl stop $name || true
-sudo -S systemctl disable $name || true
+systemctl stop $name || true
+systemctl disable $name || true
 
-sudo -S docker stop $name || true
-sudo -S docker rm -f $name || true
+docker stop $name || true
+docker rm -f $name || true

ab2653bb2714cd7e01cea75fad6fe2a99f9a171c
herjy <remyjoseph@ymail.com>
2025-03-27 22:52:02 +0100

    forwarding psql port access

--- scripts/souvenirs.sh
+++ scripts/souvenirs.sh
@@ -6,6 +6,6 @@ run_name=syno.$name
 
 bash scripts/stop_container.sh $run_name
 echo "Start container $run_name"
-sudo docker run -d --name=$run_name -p $2:$3:$3 $name $2 $3
+sudo docker run -d --name=$run_name -p 127.0.0.1:$3:$3 $name $2 $3
 
 echo "Done"

3b1126a096fa52d5cbc8af4ab37bf8c960f1dc6f
herjy <remyjoseph@ymail.com>
2025-03-27 22:48:40 +0100

    removed extra connection

--- scripts/souvenirs.sh
+++ scripts/souvenirs.sh
@@ -6,6 +6,6 @@ run_name=syno.$name
 
 bash scripts/stop_container.sh $run_name
 echo "Start container $run_name"
-sudo docker run -d --name=$run_name $name $2 $3
+sudo docker run -d --name=$run_name -p 5432:5432 $name $2 $3
 
 echo "Done"

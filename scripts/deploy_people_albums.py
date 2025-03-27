052552914a7d0281d618aa86e419e42ad6c9f7f4
herjy <remyjoseph@ymail.com>
2025-03-27 22:11:16 +0100

    removed extra connection

--- scripts/deploy_people_albums.py
+++ scripts/deploy_people_albums.py
@@ -16,7 +16,6 @@ if __name__ == '__main__':
     people = get_people(connection)
     for user in users:
 
-        connection = get_connection()
         try:
             person = people[[p.lower() == user.lower() for p in people]][0]
             album = AutoAlbum(user,

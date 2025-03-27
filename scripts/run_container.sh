068c44005a23ec21854956d0235258cb8fd5e368
herjy <remyjoseph@ymail.com>
2025-03-28 00:00:45 +0100

    full paths

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -4,7 +4,7 @@ name=$1
 echo "Build $name"
 run_name=syno.$name
 
-bash scripts/stop_container.sh $run_name
+bash /var/services/homes/remy/git_repos/photo-syn/scripts/stop_container.sh $run_name
 echo "Start container $run_name"
 sudo docker run -d --network=host --name=$run_name -p 127.0.0.1:$3:$3 $name $2 $3
 
